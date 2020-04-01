430edbe61c303a4632c754b4b1879d4de9c8c5d4
Ryno <58690325+Ryno95@users.noreply.github.com>
2020-04-01 13:49:57 +0200

    Add files via upload
    Comments added and consoles commented out

--- Sorting Site/oldCC.js
+++ Sorting Site/oldCC.js
@@ -1,23 +1,18 @@
-//name, surname, tel num, cc num, expiry date
-//only adults
-//expiry hs to be in this year after "today"
-//sort list on expiry dates
-
-// let [ day, month ] = person.birthday.dmy.split('/');
-
+//filter array on the requested year and month
 let filteredOnCCExpiry = randomPersonData.filter((person) => {
-	let [ month, year ] = person.credit_card.expiration.split('/');
+	let [ month, year ] = person.credit_card.expiration.split('/'); //make a usable date format
 	let fullYear = 20 + year;
 	let now = new Date();
 	let yearNow = now.getFullYear() + 1;
 	let monthNow = now.getMonth() + 1;
-	console.log(monthNow, yearNow);
+	// console.log(monthNow, yearNow);
 	// console.log(now);
-	console.log(month, fullYear);
+	// console.log(month, fullYear);
 	return month >= monthNow && fullYear == yearNow;
 });
-console.log(filteredOnCCExpiry);
+// console.log(filteredOnCCExpiry);
 
+//create new array with required info
 let ccExpiryArray = filteredOnCCExpiry.map((person) => {
 	return {
 		name: person.name,
@@ -28,12 +23,13 @@ let ccExpiryArray = filteredOnCCExpiry.map((person) => {
 	};
 });
 
-console.log(ccExpiryArray);
+// console.log(ccExpiryArray);
 
+//Create list with modified array and add to DOM
 let addExpiryArrayToDom = function() {
 	ccExpiryArray.forEach((person) => {
 		let listNode = document.createElement('li');
-		var nl = '\r\n';
+		var nl = '\r\n'; //to create a New Line
 		listNode.textContent =
 			'Full Name: ' +
 			person.name +
